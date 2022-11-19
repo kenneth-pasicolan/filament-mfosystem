3ebe71b4309822a5fa70584e7116c7423d174b2d
kennethkennethkenneth <kenneth.pasicolan@gmail.com>
2022-11-19 09:38:00 +0800

    second commit

--- config/filament.php
+++ config/filament.php
@@ -330,4 +330,30 @@ return [
         ],
     ],
 
+
+    const colors = require('tailwindcss/colors') 
+ 
+module.exports = {
+    content: [
+        './resources/**/*.blade.php',
+        './vendor/filament/**/*.blade.php', 
+    ],
+    darkMode: 'class',
+    theme: {
+        extend: {
+            colors: { 
+                danger: colors.rose,
+                primary: colors.blue,
+                success: colors.green,
+                warning: colors.yellow,
+            }, 
+        },
+    },
+    plugins: [
+        require('@tailwindcss/forms'), 
+        require('@tailwindcss/typography'), 
+    ],
+}
+
+
 ];
